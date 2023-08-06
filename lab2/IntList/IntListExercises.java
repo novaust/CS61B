2692079391aa7334ea70b0df44a0465cbf6ee83f
Novaust <chenrin2017@gmail.com>
2023-08-06 18:59:21 +0800

    done with lab2, fixed bug

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,11 +10,11 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
+        head.first += c;
         while (head.rest != null) {
-            head.first += c;
+            head.rest.first += c;
             head = head.rest;
         }
-        head.first += c;    // fix bug
     }
 
     /**
