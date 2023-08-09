992fc438395daf24ebebd4854919ae4e81d4e2ab
Novaust <chenrin2017@gmail.com>
2023-08-09 14:33:17 +0800

    back to buggy version

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,10 +3,12 @@
  */
 public class Collatz {
     public static int nextNumber(int n) {
-        if (n % 2 == 0) {
-            return n / 2;
-        } else {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
             return 3 * n + 1;
+        } else {
+            return n * 2;
         }
     }
 
